46c7b3c1730eb1f0f64c96e48cfa3235d3c2605d
Christian Meng <christianmeng@christians-air.lan>
2023-03-07 16:54:54 +0100

    h

---
+++ minitalk.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: christianmeng <christianmeng@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/07 16:54:36 by christianme       #+#    #+#             */
+/*   Updated: 2023/03/07 16:54:38 by christianme      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include "../libs/libs.h"
+#include <signal.h>
+#include <unistd.h>
+
+#endif
