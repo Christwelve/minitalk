86f119367223d523fcae3e8d4f998471ac28d793
Christian Meng <christianmeng@christians-air.lan>
2023-03-08 16:57:10 +0100

    added btoa to server.c

--- src/client.c
+++ src/client.c
@@ -6,37 +6,58 @@
 /*   By: christianmeng <christianmeng@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 10:37:00 by cmeng             #+#    #+#             */
-/*   Updated: 2023/03/08 16:03:57 by christianme      ###   ########.fr       */
+/*   Updated: 2023/03/08 16:39:59 by christianme      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
+void	ft_atob(int pid, char c);
 int		invalid_pid(char *str);
 int		process_exists(int pid);
-void	ft_atob(int pid, char c);
 
 
 int	main(int argc, char **argv)
 {
 	int pid;
 	int i;
-
-	pid = ft_atoi(argv[1]);
-	i = 0;
 	
-	if (argc != 3)
-		return (ft_printf("%s", "Wrong input! Use: ./client <pid> <str> "));
+	if (!(argc == 3))
+		return (ft_printf("%s\n", "Wrong input! Use: ./client <pid> <str> "));
 	if (invalid_pid(argv[1]))
-		return (ft_printf("%s", "Invalid PID"));
+		return (ft_printf("%s\n", "Invalid PID"));
+	pid = ft_atoi(argv[1]);
+	printf("%i", pid);
 	if (!process_exists(pid))
-		return (ft_printf("%s", "Process doesn't exists"));
+		return (ft_printf("%s\n", "Process doesn't exists"));
+	i = 0;
 	while (argv[2][i])
 	{
 		ft_atob(pid, argv[2][i]);
 		i++;
 	}	
 	return(0);
+}
+
+// SIGUSR1 = 0 
+// SIGUSR2 = 1
+void	ft_atob(int pid, char c)
+{
+	int bit;
+	char signal;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		signal = c & 1;
+		if(signal)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		c = c >> 1;
+		bit++;
+		usleep(100);		
+	}
 }
 
 int	invalid_pid(char *str)
@@ -58,25 +79,4 @@ int	process_exists(int pid)
 	if (kill(pid, 0) == 0)
 		return (1);
 	return (0);
-}
-
-// SIGUSR1 = 0 
-// SIGUSR2 = 1
-void	ft_atob(int pid, char c)
-{
-	int bit;
-	char signal;
-
-	bit = 0;
-	while (bit < 8)
-	{
-		signal = c & 1;
-		if(signal)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		c = c >> 1;
-		bit++;
-		usleep(500);		
-	}
 }
--- src/server.c
+++ src/server.c
@@ -6,24 +6,40 @@
 /*   By: christianmeng <christianmeng@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 10:37:08 by cmeng             #+#    #+#             */
-/*   Updated: 2023/03/07 21:30:29 by christianme      ###   ########.fr       */
+/*   Updated: 2023/03/08 16:29:35 by christianme      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
+void ft_btoa(int sig)
+{
+	static int bit;
+	static char c;
+
+	bit = 0;
+	c = 0;
+	if(sig == SIGUSR2)
+		c = c | 1;
+	c = c << 1;	
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", c);
+		bit = 0;
+		c = 0;
+	}	
+	
+}
+
 int	main(void)
 {
-	int	pid;
-
-	pid = getpid();
-
-	ft_printf("%s %i", "Server is running. Process ID:", pid);
-
+	ft_printf("%s %i", "Server is running. Process ID:", getpid());
 	while(1)
 	{
-		
+		signal(SIGUSR1, ft_btoa);
+		signal(SIGUSR2, ft_btoa);
+		sleep(1);
 	}
-
-
+	return(0);
 }
